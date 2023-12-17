44135f52450dfd0ba0d2ad0f8be734ac3a1098c6
mo <jym2013@gmail.com>
2023-12-17 16:14:31 +0900

    feat : props 관련 childern학습완료

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Button from "./Button";
 import Dice from "./Dice";
 import HandButton from "./HandButton";
 import HandIcon from "./HandIcon";
@@ -11,8 +12,13 @@ function App(){
     //     <HandButton value= 'paper' onClick={'paper'}/>
     // </div>);
     const handleClick = (value) => console.log(value);
+    const handleClearClick = () => console.log('처음부터');
     return (
     <div>
+        {/* props 을 안던져줘도 되는 방식으로 바뀜 */}
+        <Button  onClick={handleClearClick} >
+            처음부터!! 
+        </Button>
         <HandButton value2="rock" onClick={handleClick} />
         <HandButton value2="scissor" onClick={handleClick} />
         <HandButton value2="paper" onClick={handleClick} />
