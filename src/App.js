05cd60f7219bf0bd6d54c2f91217f41bc1a89db3
mo <jym2013@gmail.com>
2023-12-19 01:22:10 +0900

    type : feat state 문제풀이 완료

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import Button from "./Button";
 import Dice from "./Dice";
 import HandButton from "./HandButton";
 import HandIcon from "./HandIcon";
-
 import { compareHand, generateRandomHand } from './util';
+
+const  INITIAL_VALUE = 'rock';
 
 function getResult(me, other) {
   const comparison = compareHand(me, other);
@@ -14,40 +15,71 @@ function getResult(me, other) {
 }
 
 function App(){
-    let init = 'rock';
-
-    const [hand, setHand]= useState(init);
-    const [otherHand, setOtherHand]= useState(init);
-
-    // hand와 otherHand를 state로 바꿔 주세요
-    // const hand = 'rock';
-    // const otherHand = 'scissor';
+    const [hand, setHand]= useState(INITIAL_VALUE);
+    const [otherHand, setOtherHand]= useState(INITIAL_VALUE);
+    const [gameHistory, setGameHistory] = useState([]);
+    const [score, setScore] = useState(0);
+    const [otherScore, setOtherScore] = useState(0);
+    const [bet, setBet] = useState(1);
 
     const handleButtonClick = (nextHand) => {
-        // hand의 값을 nextHand 로 바꿔 주세요
+        const nextOtherHand = generateRandomHand();
+        const nextHistoryItem = getResult(nextHand, nextOtherHand);
+        const comparison = compareHand(nextHand, nextOtherHand);
         setHand(nextHand);
-        // otherHand의 값을 generateRandomHand()의 리턴 값으로 바꿔주세요
-        setOtherHand(generateRandomHand());
+        setOtherHand(nextOtherHand);
+        setGameHistory([...gameHistory, nextHistoryItem]); //배열로!!!
+
+        //parseInt 대신에 Number() 생성자로 처음부터 넘겨주기 가능함
+        if (comparison > 0) setScore(score + bet);
+        if (comparison < 0) setOtherScore(otherScore + bet);
     };
 
     const handleClearClick = () => {
-        // hand와 otherHand의 값을 'rock' 으로 바꿔주세요
-        setHand(init);
-        setOtherHand(init)
+        setHand(INITIAL_VALUE);
+        setOtherHand(INITIAL_VALUE)
+        // gameHistory를 비워주세요
+        setGameHistory([]);
+        setScore(0);
+        setOtherScore(0);
+        setBet(1);
     };
 
+    const handleBetChange = (e) => {
+        // if(e.target.value === '')
+        //     setBet(5);
+        // if(!(0 < e.target.value || e.target.value < 10)) {
+            //     setBet(9)
+            //     return
+            // };
 
-    // const [value, setValue]= useState('scissor');
+        let num = Number(e.target.value); //정수 문자열의 빈 문자열은 0임
+        if(num > 9) num %= 10;
+        if(num < 1) num = 1;
+        num = Math.floor(num);
+        setBet(num);
+      };
 
     return (
     <div>
-        <Button onClick={handleClearClick}>처음부터</Button>
+        <Button onClick={handleClearClick}>
+            처음부터
+        </Button>
         <p>{getResult(hand, otherHand)}</p>
+        <div>
+            {score} : {otherScore}
+        </div>
         <div>
             <HandIcon value={hand} />
             VS
             <HandIcon value={otherHand} />
         </div>
+        <div>
+            <input onChange={handleBetChange} type="number" value={bet} min={1} max={9}></input>
+        </div>
+        <p>
+            승부 기록: {gameHistory.join(', ')}
+        </p>
         <div>
             <HandButton value="rock" onClick={handleButtonClick} />
             <HandButton value="scissor" onClick={handleButtonClick} />
