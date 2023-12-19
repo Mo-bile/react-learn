b4e701168f4b8893dcc7642ca8879e77bf18bb39
mo <jym2013@gmail.com>
2023-12-20 00:01:54 +0900

    feat : 인라인스타일 학습완료

--- src/HandButton.js
+++ src/HandButton.js
@@ -1,8 +1,24 @@
 import HandIcon from "./HandIcon";
+import purpleImage from './assets/purple.svg'
+
+const HandButtonStyle = {
+    width: '166px',
+    height: '166px',
+    border: 'none',
+    outline: 'none',
+    textAlign: 'center',
+    cursor: 'pointer',
+    backgroundColor: 'transparent',
+    backgroundImage: `url(${purpleImage})`, //이런식으로 넣어주어야함
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: 'center',
+    backgroundSize: 'contain'
+};
+
 function HandButton({ value, onClick }) {
     const handleClick = () => onClick(value);
     return (
-        <button onClick={handleClick}>
+        <button style={HandButtonStyle} onClick={handleClick}>
             <HandIcon value={value}/>
         </button>);
   }
