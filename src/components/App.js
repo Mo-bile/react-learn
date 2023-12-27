ba7d6277620b492382daa85cdb58370114a3daee
mo <jym2013@gmail.com>
2023-12-28 01:27:12 +0900

    type : setting idea 관련 gitIgnore 추가 완료

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import "./FoodList.css";
 import items from "../mock.json";
 import FoodList from "./FoodList";
+import { useState } from "react";
 
 function App() {
+  const [order, setOrder] = useState("createdAt");
+  const sortedItem = items.sort((a, b) => b[order] - a[order]);
+  const handleNewsClick = () => setOrder("createdAt");
+  const handleCalorieClick = () => setOrder("calorie");
+
   return (
     <div className="FoodListItem">
-      <FoodList items={items} />
+      <button onClick={handleNewsClick}>최신순</button>
+      <button onClick={handleCalorieClick}>칼로리순</button>
+      <FoodList items={sortedItem} />
     </div>
   );
 }
--- src/components/FoodList.js
+++ src/components/FoodList.js
@@ -1,12 +1,18 @@
+function formatData(value) {
+  const date = new Date(value);
+  return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
+}
+
 function FoodListItem({ item }) {
   const { imgUrl, title, calorie, content } = item;
 
   return (
     <div>
       <img src={imgUrl} alt={title} className="FoodListItem-img" />
-      <div>{title}</div>
-      <div>{calorie}</div>
-      <div>{content}</div>
+      <div>제목 : {title}</div>
+      <div>칼로리 : {calorie}</div>
+      <div>내용 : {content}</div>
+      <div>{formatData(item.createdAt)}</div>
     </div>
   );
 }
