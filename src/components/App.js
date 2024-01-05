9bc0d04d90420354f5d7ea0edda66a7493b90b78
mo <jym2013@gmail.com>
2024-01-06 00:23:02 +0900

    type : filter() 기능 실습

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,24 @@
 import ReviewList from "./ReviewList";
-import items from "../mock.json";
+import mockItems from "../mock.json";
 import { useState } from "react";
 
 function App() {
+const [items, setItems] = useState(mockItems);
   const [order, setOrder] = useState("createdAt");
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
   const handleNewestClick = () => setOrder("createdAt");
   const handleBestClick = () => setOrder("rating");
+    const handleDelete = (id) => {
+        const nextItems = items.filter((item) => item.id !== id);
+        setItems(nextItems);
+    }
 
   return (
     <div>
       <button onClick={handleNewestClick}>createdAt</button>
       <button onClick={handleBestClick}>rating</button>
-      <ReviewList items={sortedItems} />
+      <ReviewList items={sortedItems} onDelete={handleDelete}/>
     </div>
   );
 }
