de02e815fe574f8e5c2a597ec199066591da98ce
mo <jym2013@gmail.com>
2024-01-06 12:24:11 +0900

    type : useeffect 방식 실습

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,36 @@
 import ReviewList from "./ReviewList";
 // import mockItems from "../mock.json";
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import {getReviews} from "../api";
 
 function App() {
-const [items, setItems] = useState([]);
-  const [order, setOrder] = useState("createdAt");
-  const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
-  const handleNewestClick = () => setOrder("createdAt");
-  const handleBestClick = () => setOrder("rating");
+    const [items, setItems] = useState([]);
+    const [order, setOrder] = useState("createdAt");
+    const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+    const handleNewestClick = () => setOrder("createdAt");
+    const handleBestClick = () => setOrder("rating");
     const handleDelete = (id) => {
         const nextItems = items.filter((item) => item.id !== id);
         setItems(nextItems);
-    }
-    const handleLoadCLick = async () => {
+    };
+    const handleLoad = async () => {
         const {reviews} = await getReviews();
         setItems(reviews);
     };
 
-  return (
+    useEffect(() =>{
+        handleLoad();
+    },[]);
+
+    return (
     <div>
-      <button onClick={handleNewestClick}>createdAt</button>
-      <button onClick={handleBestClick}>rating</button>
-      <ReviewList items={sortedItems} onDelete={handleDelete}/>
-        <button onClick={handleLoadCLick}>불러오기</button>
+        <button onClick={handleNewestClick}>createdAt</button>
+        <button onClick={handleBestClick}>rating</button>
+        <ReviewList items={sortedItems} onDelete={handleDelete}/>
     </div>
-  );
+    );
 }
 
 export default App;
